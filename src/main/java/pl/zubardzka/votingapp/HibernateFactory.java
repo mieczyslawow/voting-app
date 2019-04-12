97e17af99b2559c31b61034edbad42c0c247330f
mieczyslawow <lukaszmajchrzak@onet.eu>
2019-04-12 23:10:32 +0200

    Initial commit

---
+++ src/main/java/pl/zubardzka/votingapp/HibernateFactory.java
@@ -0,0 +1,16 @@
+package pl.zubardzka.votingapp;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateFactory {
+
+    public SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration().configure();
+        StandardServiceRegistryBuilder registryBuilder =
+                new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        SessionFactory sessionFactory = configuration.buildSessionFactory(registryBuilder.build());
+        return sessionFactory;
+    }
+}
